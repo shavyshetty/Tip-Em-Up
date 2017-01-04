9dfc8457e5abe0b09dffbce5020578678f2e26a9
shavyshetty <sharvil.14391@gmail.com>
2017-01-05 01:49:25 +0530

    First Commit

--- TipEmUp/ViewController.swift
+++ TipEmUp/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textBillAmount: UITextField!
+    @IBOutlet weak var textTipAmount: UITextField!
+    @IBOutlet weak var textSelector: UISegmentedControl!
+    
+    @IBOutlet weak var textTotalAmount: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func onBillAmountChange(_ sender: Any) {
+     
+     let TipPercentages = [0.10, 0.15, 0.20]
+        
+     let TotalAmount = Double(textBillAmount.text!) ?? 0
+     let FinalTip =    TotalAmount * TipPercentages[textSelector.selectedSegmentIndex]
+     textTipAmount.text = String(format: "$%.2f", FinalTip)
+     textTotalAmount.text = String(format: "$%.2f",TotalAmount + FinalTip)
+    }
 
-
+    @IBAction func onSelectorValueChange() {
+        onBillAmountChange(self)
+    }
 }
+
 
